15da65fc91acf8c80b2fe41f6f84fa33df578af3
Marxism-Doc <128950573+Marxism-Doc@users.noreply.github.com>
2023-07-31 00:10:08 +0800

    [feat] font storage

--- docs/src/javascripts/custom.js
+++ docs/src/javascripts/custom.js
@@ -2,4 +2,20 @@ function changeFont() {
 	const fontSelector = document.getElementById("font-selector");
 	const selectedFont = fontSelector.value;
 	document.body.style.fontFamily = selectedFont;
+	localStorage.setItem("selectedFont", selectedFont);
 }
+
+function applyFont(storedFont) {
+	document.body.style.fontFamily = storedFont;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	const storedFont = localStorage.getItem("selectedFont");
+	if (storedFont) {
+		const fontSelector = document.getElementById("font-selector");
+		if (fontSelector) {
+			fontSelector.value = storedFont;
+		}
+		applyFont(storedFont);
+	}
+});
